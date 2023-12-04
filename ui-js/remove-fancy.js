e970e3fce37b4c40b8d7ac5e272d59facb9519fb
davebrogan <dave@davebrogan.com>
2023-12-03 17:27:04 -0700

    added fancy link modifiers

---
+++ ui-js/remove-fancy.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('<.parent-element-class-name>').find('.fancy-link').removeClass('fancy-link');
+    $('<.parent-element-class-name>').find('.fancy-link').removeClass('fancy-link');
+});
+
+//place at end of body html
+//parent-element-class-name = name of the parent class in which the fancy-link tab should be removed
+//use in conjunction with the class styles .fancy-link-white (on the a element
+//or with personnel-card__content--dark on the container
